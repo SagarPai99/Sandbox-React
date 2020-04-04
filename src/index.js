8dafd24f7287a488f01d9a28737dc030f08cd165
Sagar Sudesh Pai <sagar.sudesh.pai@gmail.com>
2020-04-04 12:08:59 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+//Import and exporting in es6 feature
+//var React = require("react");
+//var ReactDOM = require("react-dom");
+
+ReactDOM.render(
+  <div>
+    <h1>Hello World!</h1>
+    <p>This is a pragraph</p>
+  </div>,
+  document.getElementById("root")
+);
+//Converts to  Browser Compatible JavaScript
+
+// var h1 = document.createElement("h1");
+// h1.innerHTML = "Hello World!";
+// document.getElementById("root").appendChild(h1);
